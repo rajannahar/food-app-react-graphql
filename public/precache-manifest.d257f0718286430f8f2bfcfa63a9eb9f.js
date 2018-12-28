8a47e60f179afdee0b88b43b2527f6d2a642207e
R_N101 <R_N101@msn.com>
2018-12-28 21:29:36 +0000

    public build

---
+++ public/precache-manifest.d257f0718286430f8f2bfcfa63a9eb9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5fed7f265dd65518714d",
+    "url": "/static/js/main.5fed7f26.chunk.js"
+  },
+  {
+    "revision": "a088e6031fa9528ca596",
+    "url": "/static/js/1.a088e603.chunk.js"
+  },
+  {
+    "revision": "5fed7f265dd65518714d",
+    "url": "/static/css/main.f76061b3.chunk.css"
+  },
+  {
+    "revision": "8e56ca2efb195769050a025bd5f2782c",
+    "url": "/index.html"
+  }
+];
