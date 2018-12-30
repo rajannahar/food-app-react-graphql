118e51823d8a9ea1bd58f8235fe22f44ec5d31e5
R_N101 <R_N101@msn.com>
2018-12-30 15:52:20 +0000

    heroku - deployment

---
+++ public/precache-manifest.2d3b8ecba10507d2d98a903d6fedbc85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9743ac22b83fccb9f865",
+    "url": "/static/js/main.9743ac22.chunk.js"
+  },
+  {
+    "revision": "d2c9ebafac789d2aba93",
+    "url": "/static/js/1.d2c9ebaf.chunk.js"
+  },
+  {
+    "revision": "9743ac22b83fccb9f865",
+    "url": "/static/css/main.1e3c132c.chunk.css"
+  },
+  {
+    "revision": "ccee87d874665d9f26020249351c1e7b",
+    "url": "/index.html"
+  }
+];
